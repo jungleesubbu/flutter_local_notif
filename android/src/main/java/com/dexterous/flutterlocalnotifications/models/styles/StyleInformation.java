89f74aab4c1e83838019973cea93d12f31e9a4b0
Subrahmanyam Pampana <subrahmanyampampana@MAC-ENG-PAMPANASUBBU.local>
2021-08-24 09:48:13 +0530

    first commit

---
+++ android/src/main/java/com/dexterous/flutterlocalnotifications/models/styles/StyleInformation.java
@@ -0,0 +1,9 @@
+package com.dexterous.flutterlocalnotifications.models.styles;
+
+import androidx.annotation.Keep;
+
+import java.io.Serializable;
+
+@Keep
+public abstract class StyleInformation  implements Serializable {
+}
